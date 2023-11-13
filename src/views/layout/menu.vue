fc46b3b7a38a024380d3751e2bf398ec9bdf434f
simtek/yangrui <17368465776@163.com>
2023-11-13 10:05:04 +0800

    绘图渲染

--- src/views/layout/menu.vue
+++ src/views/layout/menu.vue
@@ -2,7 +2,7 @@
  * @Author: simtek/yangrui 17368465776@163.com
  * @Date: 2023-09-11 09:38:23
  * @LastEditors: simtek/yangrui 17368465776@163.com
- * @LastEditTime: 2023-11-07 14:02:37
+ * @LastEditTime: 2023-11-13 10:04:40
  * @FilePath: \irelia-frontend\src\views\layout\menu.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -29,6 +29,10 @@ const menus = [
   {
     url: "/dom",
     label: "DOM 解析",
+  },
+  {
+    url: "/render",
+    label: "绘图渲染",
   },
 ];
 
