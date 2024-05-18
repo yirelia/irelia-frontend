d793c4a3cc0f50a2caba09247a8e33e1706d489b
simtek/yangrui <17368465776@163.com>
2024-05-18 10:26:48 +0800

    feat；路径

---
+++ src/graphs/paths/direc-df-path.ts
@@ -0,0 +1,26 @@
+import { Diagraph } from "../graph";
+export class DirectDFPaths {
+  private marked: boolean[];
+  constructor(graph: Diagraph, source: number | number[]) {
+    this.marked = Array.from({ length: graph.V }, () => false);
+    if (Array.isArray(source)) {
+      source.forEach((v) => {
+        if (!this.marked[v]) {
+          this.dfs(graph, v);
+        }
+      });
+    } else {
+      this.dfs(graph, source);
+    }
+  }
+
+  private dfs(graph: Diagraph, v: number) {
+    this.marked[v] = true;
+    const vs = graph.adjToArray(v);
+    vs.forEach((item) => {
+      if (!this.marked[item]) {
+        this.dfs(graph, item);
+      }
+    });
+  }
+}
--- src/graphs/paths/index.ts
+++ src/graphs/paths/index.ts
@@ -1,44 +1,53 @@
 import { Bag } from "../data-struct";
-import { readTinyCG, readTinyG } from "../graph";
+import { initDiagraphTindyDG, readTinyCG, readTinyG } from "../graph";
+import { Diagraph } from "../graph/diagraph";
 import { BreadthFirstPaths } from "./breadth-path";
 import { CC } from "./cc";
 import { DepthFirstPaths } from "./dep-path";
+import { DirectDFPaths } from "./direc-df-path";
 export function pathTo() {
-    const graph = readTinyCG()
-    const depthPath = new DepthFirstPaths(graph, 0)
-    for(let i = 0; i < graph.V; i++) {
-        const paths = depthPath.pathTo(i)
-        console.log(`0 to ${i} : ${paths?.collention.reverse().join('-')}`)
-    }
+  const graph = readTinyCG();
+  const depthPath = new DepthFirstPaths(graph, 0);
+  for (let i = 0; i < graph.V; i++) {
+    const paths = depthPath.pathTo(i);
+    console.log(`0 to ${i} : ${paths?.collention.reverse().join("-")}`);
+  }
 }
 
 export function breadPathTo() {
-    const graph = readTinyCG()
-    const depthPath = new BreadthFirstPaths(graph, 0)
-    for(let i = 0; i < graph.V; i++) {
-        const paths = depthPath.pathTo(i)
-        console.log(`0 to ${i} : ${paths?.collention.reverse().join('-')}`)
-    }
+  const graph = readTinyCG();
+  const depthPath = new BreadthFirstPaths(graph, 0);
+  for (let i = 0; i < graph.V; i++) {
+    const paths = depthPath.pathTo(i);
+    console.log(`0 to ${i} : ${paths?.collention.reverse().join("-")}`);
+  }
 }
 
 export function prinCcc() {
-    const graph = readTinyG()
-    const c = new CC(graph)
-    console.log('===== cc ======')
-    console.log(`${c.count} components`)
-    const components = Array.from({length: c.count}, () => new Bag<number>())
-    for(let i = 0; i < graph.V; i++) {
-        components[c.id[i]].addItem(i)
+  const graph = readTinyG();
+  const c = new CC(graph);
+  console.log("===== cc ======");
+  console.log(`${c.count} components`);
+  const components = Array.from({ length: c.count }, () => new Bag<number>());
+  for (let i = 0; i < graph.V; i++) {
+    components[c.id[i]].addItem(i);
+  }
+  for (let i = 0; i < c.count; i++) {
+    let item = components[i];
+    let s = "";
+    let head = item.head;
+    while (head) {
+      s += head.val + " ";
+      head = head.next;
     }
-    for(let i =0 ; i< c.count; i++) {
-        let item = components[i]
-        let s = ''
-        let head = item.head
-        while(head) {
-            s += head.val + ' '
-            head = head.next
-        }
-        console.log(s)
-    }
+    console.log(s);
+  }
+}
 
+export function testDirectDFPaths() {
+  const graph = initDiagraphTindyDG();
+  console.log("===== diagraph paths ======");
+  const reachable = new Diagraph(1);
+  const directDfPath = new DirectDFPaths(graph, 1);
+  console.log(directDfPath);
 }
