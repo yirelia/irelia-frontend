f27d38b0d6f5e511b352f7659cc641c67cdd9ab8
simtek/yangrui <17368465776@163.com>
2024-07-21 15:30:31 +0800

    添加节点

--- src/views/fault-tree/index.vue
+++ src/views/fault-tree/index.vue
@@ -1,6 +1,20 @@
 <template>
   <el-container>
     <el-aside width="300px"></el-aside>
-    <el-main></el-main>
+    <el-main>
+      <div ref="faultTreeRef"></div>
+    </el-main>
   </el-container>
 </template>
+<script setup lang="ts">
+import { Graph } from "@antv/x6";
+import { onMounted, ref } from "vue";
+
+const faultTreeRef = ref<HTMLElement>();
+const graph = ref<Graph>();
+onMounted(() => {
+  graph.value = new Graph({
+    container: faultTreeRef.value,
+  });
+});
+</script>
