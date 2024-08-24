6b0bf6fbd375c640eaa33a11c494ecf960bdcf21
rui <17368465776@163.com>
2024-08-24 18:50:30 +0800

    支持飞机飞行

--- src/views/learn-three/chart-01/index.vue
+++ src/views/learn-three/chart-01/index.vue
@@ -8,14 +8,17 @@
     // 相机控件
     import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
     import { onMounted, ref } from "vue";
-    import * as dat from 'dat.gui';
     import { useDirectionLight } from "./directlight.hook";
-    import { usePlaneGui } from "./dat.hook";
+    import { useCatmullRomCurve3 } from "./three.hooks";
     import { Matrix4 } from "three";
-    import { useControl } from "./control.hook";
 
-    const planePosition = new THREE.Vector3(-10, 4, 0)
+    const planePosition = new THREE.Vector3(-10, 10, 0)
 
+    let yaw = 0 // 
+    let pictch = 0
+    let roll = 0
+
+    let i = 0
     const helixRef = ref<THREE.Object3D>()
 
     const planeRef = ref<THREE.Group<THREE.Object3DEventMap>>()
@@ -41,12 +44,12 @@
         window.addEventListener("keyup", (e) => {
             Keys[e.key.toLowerCase()] = false;
         });
+
+        const { curveObject, points } = useCatmullRomCurve3()
         const renderer = createRenderer(domRef.value as HTMLDivElement)
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(50, domRef.value!.offsetWidth / domRef.value!.offsetHeight, 0.1, 1000);
-        camera.position.set(-30, 60, 0)
-        // const cameraHelper = new THREE.CameraHelper(camera);
-        // scene.add(cameraHelper)
+        camera.position.set(50, 90, 90)
         camera.lookAt(scene.position)
         domRef.value?.appendChild(renderer.domElement)
         // 辅助辅助坐标轴
@@ -54,93 +57,80 @@
         scene.add(axesHelper)
 
         // 平面
-        const planeGeometry = new THREE.PlaneGeometry(100, 50);
+        const planeGeometry = new THREE.PlaneGeometry(100, 60);
         const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
         const plane = new THREE.Mesh(planeGeometry, planeMaterial);
         plane.castShadow = true;
         plane.receiveShadow = true;
         plane.rotation.x = -0.5 * Math.PI;
-        plane.position.set(10, 0, 0);
+        plane.position.set(10, -100, 0);
         scene.add(plane);
-
-        const sphereGeometry = new THREE.SphereGeometry(60, 50, 50)
-        const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-        sphereMaterial.map = new THREE.TextureLoader().load('assets/textures/envmap.jpg')
-        sphereMaterial.side = THREE.BackSide
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        scene.add(sphere)
-
-
-        const loader = new GLTFLoader();
-        loader.load('assets/models/airplane.glb', (data) => {
-            planeRef.value = data.scene
-            scene.add(data.scene)
-            data.scene.rotation.y = THREE.MathUtils.degToRad(90)
-            data.scene.name = 'airplane'
-            console.log(data.scene)
-            helixRef.value = data.scene.getObjectByName('helix')
-            console.log(helixRef.value, planeRef.value)
-            render()
-
-        })
+        // 添加灯光
         const { directionLight } = useDirectionLight()
         scene.add(directionLight)
 
-        let xRot = 0
-
         function render() {
-            // const plane = scene.getObjectByName('airplane')
-
             if (planeRef.value) {
-                if (Keys['w']) {
-                    planePosition.add(new THREE.Vector3(0.1, 0.1, 0))
-
-                }
-                if (Keys['s']) {
-                    planePosition.add(new THREE.Vector3(-0.1, -0.1, 0))
-                }
                 if (Keys['a']) {
-                    xRot -= 10
+                    roll += 0.01
                 }
                 if (Keys['d']) {
-                    xRot += 10
+                    roll -= 0.01
                 }
 
-                console.log(planePosition)
-                // 飞机头正向x轴
-                const rotationMatrix = new THREE.Matrix4().makeRotationY(THREE.MathUtils.degToRad(90))
-                const xRotMartix = new THREE.Matrix4().makeRotationZ(THREE.MathUtils.degToRad(xRot))
-                const matrix = new THREE.Matrix4().multiply(new THREE.Matrix4()
-                    .makeTranslation(planePosition.x, planePosition.y, planePosition.z))
-                    .multiply(rotationMatrix).multiply(xRotMartix);
+                if (Keys['w']) {
+                    pictch += 0.01
+                }
+                if (Keys['s']) {
+                    pictch -= 0.01
+                }
+                if (Keys['q']) {
+                    yaw += 0.01
+                }
+                if (Keys['e']) {
+                    yaw -= 0.01
+                }
+                let position = new THREE.Vector3()
+                if (i < points.length) {
+                    position = points[i]
+                    i++
+                } else {
+                    i = 0
+                }
+                const rotationMatrix = new Matrix4().makeRotationFromEuler(new THREE.Euler(roll, yaw, pictch, 'ZYX'))
+                const translationMatrix = new Matrix4().makeTranslation(position.x, position.y, position.z)
+                const matrix = new Matrix4().multiply(translationMatrix).multiply(rotationMatrix)
+
                 console.log(matrix)
-                planeRef.value.matrixAutoUpdate = false;
-                planeRef.value.matrix.copy(matrix);
-                planeRef.value.matrixWorldNeedsUpdate = true;
+                // const matrix = new THREE.Matrix4().multiply(position).makeTranslation().makeTranslation(new THREE.Vector3(1, 0, 0)))
+                planeRef.value.matrixAutoUpdate = false
+                planeRef.value.matrix.copy(matrix)
+                planeRef.value.matrixWorldNeedsUpdate = true
 
             }
 
-            // const cameraMatrix = new THREE.Matrix4()
-            //     .multiply(new Matrix4().makeTranslation(planePosition.x, planePosition.y, planePosition.z))
-            //     .multiply(delayedRotMatrix)
-            //     .multiply(new Matrix4().makeRotationX(-0.2))
-            //     .multiply(
-            //         new Matrix4().makeTranslation(0, 0.015, 0.3)
-            //     );
-
-            // camera.matrixAutoUpdate = false;
-            // camera.matrix.copy(cameraMatrix);
-            // camera.matrixWorldNeedsUpdate = true;
-
-
-            if (helixRef.value) {
-                helixRef.value.rotation.z += 1
-            }
-
-            renderer.render(scene, camera);
-
+            renderer.render(scene, camera)
         }
 
+        function genPosition() {
+            const x = Math.random() * 100 - 50
+            const y = Math.random() * 100 - 50
+            const z = Math.random() * 100 - 50
+            return new THREE.Vector3(x, y, z)
+        }
+
+
+        const loader = new GLTFLoader();
+        loader.load('assets/models/airplay-zyx.glb', (data) => {
+            planeRef.value = data.scene
+            scene.add(data.scene)
+            data.scene.name = 'airplane'
+            data.scene.position.copy(planePosition)
+            renderer.setAnimationLoop(render);
+        })
+
+
+        scene.add(curveObject);
 
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.enableZoom = true;
@@ -150,31 +140,7 @@
             renderer.render(scene, camera)
         })
 
-        const gui = new dat.GUI()
-
-        const params = {
-            a: false,
-            s: false,
-            d: false,
-            w: false
-        }
-
-        gui.add(params, 'a').onChange((v) => {
-            Keys['a'] = v
-        })
-
-        gui.add(params, 's').onChange((v) => {
-            Keys['s'] = v
-        })
-
-        gui.add(params, 'd').onChange((v) => {
-            Keys['d'] = v
-        })
-        gui.add(params, 'w').onChange((v) => {
-            Keys['w'] = v
-        })
-
-        renderer.setAnimationLoop(render);
+        // render()
 
     })
 
