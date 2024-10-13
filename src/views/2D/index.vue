db2ce45717851f26d5476962993efa7cda8cd81d
rui <17368465776@163.com>
2024-10-13 20:29:23 +0800

    feat:支持翻转

--- src/views/2D/index.vue
+++ src/views/2D/index.vue
@@ -6,6 +6,7 @@
 <script lang='ts' setup>
     import { Graph } from '@antv/x6/es';
     import { onMounted, ref } from 'vue';
+    import * as glMatrix from 'gl-matrix';
     const x6Instance = ref()
     onMounted(() => {
         const graph = new Graph({
@@ -52,6 +53,52 @@
         graph.addEdge({
             source: { x: 0, y: -100 },
             target: { x: 0, y: 100 },
+        })
+
+        graph.addNode({
+            x: 100,
+            y: 100,
+            width: 100,
+            height: 100,
+            attrs: {
+            }
+        })
+
+        // const matrix = new DOMMatrix()
+        const rad = glMatrix.glMatrix.toRadian(45)
+
+        const m1 = glMatrix.mat2d.create()
+        const m2 = glMatrix.mat2d.create()
+        const tm = glMatrix.mat2d.fromValues(1, 0, 0, 1, -50, -50)
+        const rM = glMatrix.mat2d.create()
+        glMatrix.mat2d.rotate(rM, rM, rad)
+        const tm1 = glMatrix.mat2d.fromValues(1, 0, 0, 1, 50, 50)
+        const sm = glMatrix.mat2d.fromValues(-1, 0, 0, 1, 0, 0)
+
+        glMatrix.mat2d.multiply(m1, tm, m1)
+        glMatrix.mat2d.multiply(m1, rM, m1)
+        glMatrix.mat2d.multiply(m1, tm1, m1)
+        glMatrix.mat2d.multiply(m1, sm, m1)
+        console.log(m1)
+        const mt = `matrix(${m1[0]}, ${m1[1]}, ${m1[2]}, ${m1[3]}, ${m1[4]}, ${m1[5]})`
+
+
+
+        graph.addNode({
+            x: 100,
+            y: 100,
+            width: 100,
+            height: 100,
+            attrs: {
+                body: {
+                    fill: 'red',
+                    stroke: 'blue',
+                    strokeWidth: 2,
+                    rx: 10,
+                    ry: 10,
+                    transform: mt,
+                }
+            }
         })
 
         graph.centerPoint(0, 0)
